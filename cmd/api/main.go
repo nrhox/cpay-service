e15ad4778a2a9fe4ff96d8721d9e96a91ae8ceba
Jalu <jalunugroho118@gmail.com>
2026-06-06 14:13:24 +0700

    feat(cmd/api): create connection database and create api endpoint

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,57 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log/slog"
+	"net/http"
+	"os"
+	"runtime"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/nrhox/cpay-service/internal/app"
+	"github.com/nrhox/cpay-service/internal/config"
+	"github.com/nrhox/cpay-service/pkg/database"
+)
 
 func main() {
-	fmt.Println("halo")
+	cfg := config.Load()
+
+	dbMongo, disconnent, err := database.NewMongoConnetion(*cfg)
+	if err != nil {
+		panic(err)
+	}
+	defer disconnent(context.Background())
+
+	log := config.LoadLogger(cfg)
+	log.Info(
+		"application run",
+		slog.Attr{
+			Key:   "pid",
+			Value: slog.IntValue(os.Getpid()),
+		},
+		slog.Attr{
+			Key:   "os",
+			Value: slog.StringValue(runtime.GOOS),
+		},
+		slog.Attr{
+			Key:   "arch",
+			Value: slog.StringValue(runtime.GOARCH),
+		},
+	)
+
+	r := chi.NewRouter()
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("hello"))
+	})
+
+	appBootstrap := app.Bootstrap{
+		Route:  r,
+		DB:     dbMongo,
+		Logger: log,
+		Cfg:    cfg,
+	}
+	appBootstrap.Init()
+	appBootstrap.PrintAllRoute()
+
+	http.ListenAndServe(cfg.AppPort, r)
 }
