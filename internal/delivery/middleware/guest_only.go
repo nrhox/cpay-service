ff75e712406ecbebf7d5a3e75bf2354977b09fb7
Jalu <jalunugroho118@gmail.com>
2026-06-09 14:54:06 +0700

    feat(middleware): validation response type redirect if provider no inject

--- internal/delivery/middleware/guest_only.go
+++ internal/delivery/middleware/guest_only.go
@@ -3,6 +3,7 @@ package middleware
 import (
 	"net/http"
 
+	"github.com/go-chi/chi/v5"
 	"github.com/nrhox/cpay-service/pkg/response"
 	"github.com/nrhox/cpay-service/pkg/security"
 	"go.mongodb.org/mongo-driver/v2/bson"
@@ -10,11 +11,18 @@ import (
 
 func (m *Middlware) GuestOnly(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		isInject := chi.URLParam(r, "provider") == "inject"
+
 		token, id := security.GetRefreshToken(r)
 		if token != "" || id != bson.NilObjectID {
-			response.Json(w, http.StatusOK, response.ResJson{
-				Message: "You are already logged in",
-			})
+			if isInject {
+				response.Json(w, http.StatusOK, response.ResJson{
+					Message: "You are already logged in",
+				})
+				return
+			}
+
+			http.Redirect(w, r, m.config.FrontendUrl, http.StatusTemporaryRedirect)
 			return
 		}
 
