1647a1300eb4f01307eeb012f4fab6b38a3c97a1
Jalu <jalunugroho118@gmail.com>
2026-06-09 14:52:56 +0700

    feat(route): remove middleware guest only in route /auth/{provider}/callback

--- internal/delivery/route/route.go
+++ internal/delivery/route/route.go
@@ -22,7 +22,7 @@ func NewRoute(
 ) {
 	r.Route("/api/auth", func(r chi.Router) {
 		r.With(m.GuestOnly).Get("/{provider}", authH.Login)
-		r.With(m.GuestOnly, m.InjectUser).Get("/{provider}/callback", authH.Callback)
+		r.With(m.InjectUser).Get("/{provider}/callback", authH.Callback)
 		r.With(m.IsAuth(true)).Post("/incomplate", authH.IncomplateRegister)
 		r.With(m.IsAuth(true)).Get("/__refresh", authH.RefreshToken)
 		r.With(m.IsAuth(false)).Get("/logout", authH.Logout)
